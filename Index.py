87118652de2a3305c11a50e67f6e9cf46cf0553e
moreym26 <moreym26@up.edu>
2024-11-13 11:44:15 -0800

    Cleaning things up for video demo

--- Index.py
+++ Index.py
@@ -4,17 +4,20 @@
 
 # opening the file in read mode 
 file = open("Test.txt", "r") 
-  
 # reading the file 
 data = file.read() 
-  
-# replacing end splitting the text  
-# when newline ('\n') is seen. 
+
+# Label what is being printed
+print('The formal description your input is: ')
+
+# When theres a newline ('\n'), make new element. 
 formalDesc = data.split("\n") 
-#Printing elements into list
+
+# Printing elements into list
 print(formalDesc) 
 file.close() 
 
+# Test case Demo
 if formalDesc[0] == 'q0 q1 q2':
     print('Its in proper form')
 

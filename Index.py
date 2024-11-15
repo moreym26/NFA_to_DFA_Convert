4d7884a51e6bf6e194acf350cef5b6a3e717ad7c
moreym26 <moreym26@up.edu>
2024-11-14 16:33:40 -0800

    cplit nfa description into mini arrays

--- Index.py
+++ Index.py
@@ -2,17 +2,23 @@
 # Main file with code
 # By Malory Morey
 
-# opening the file in read mode 
+# Openingthe file in read mode 
 file = open("Test.txt", "r") 
-# reading the file 
 data = file.read() 
 
 # Print a newline to make output look nice and label what is being printed
 print('\n')
 print('The formal description of your NFA is: ')
 
+
+#  ---------------Creating elements of NFA formal description---------------
 # When theres a newline ('\n'), make new element. 
 nfaDesc = data.split("\n") 
+# Split formal description into their own arrays
+nfaStates = nfaDesc[0].split(' ')
+nfaAlpha = nfaDesc[1].split(' ')
+nfaStart = nfaDesc[3].split(' ')
+nfaEnd = nfaDesc[4].split(' ')
 
 # Printing elements into list
 print(nfaDesc) 
@@ -21,12 +27,11 @@ file.close()
 # Test case Demo
 if nfaDesc[0] == 'q0 q1 q2':
     print('Its in proper form')
-
-# Print a newline to make output look nice
+# ---------------Printing elements of DFA formal description---------------
+# Print a newline to make output look nice and label what is being printed
 print('\n')
-
-# Label what is being printed
 print('The formal description of your DFA is: ')
+
 # Dummied up for demo
 dfaDesc = nfaDesc
 print(dfaDesc) 

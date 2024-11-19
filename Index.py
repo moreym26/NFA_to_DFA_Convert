f7fabcd36d3e7cb106c5a04909ef2049d60a4f21
moreym26 <moreym26@up.edu>
2024-11-19 11:59:37 -0800

    trying to import numpy

--- Index.py
+++ Index.py
@@ -1,3 +1,6 @@
+#Imports
+import numpy
+
 # NFA -> DFA Converter
 # Main file with code
 # By Malory Morey
@@ -14,11 +17,12 @@ print('The formal description of your NFA is: ')
 #  ---------------Creating elements of NFA formal description---------------
 # When theres a newline ('\n'), make new element. 
 nfaDesc = data.split("\n")
+
 # Split formal description into their own arrays
-nfaStates = nfaDesc[0].split(' ')
-nfaAlpha = nfaDesc[1].split(' ')
-nfaStart = nfaDesc[3].split(' ')
-nfaEnd = nfaDesc[4].split(' ')
+nfaStates = numpy.array(i, nfaDesc[0].split(' '))
+nfaAlpha = numpy.array(1, nfaDesc[1].split(' '))
+nfaStart = numpy.array(nfaDesc[3].split(' '))
+nfaEnd = numpy.array(nfaDesc[4].split(' '))
 
 # Printing elements into list
 print(nfaDesc) 
@@ -27,17 +31,23 @@ file.close()
 # Test case Demo
 if nfaDesc[0] == 'q0 q1 q2':
     print('Its in proper form')
+# ---------------Converting elements of DFA formal description---------------
+#Loop through states to make nfa states
+for i in nfaStates:
+    #dfaStates[i] = nfaStates[i]
+    #if i == nfaStates.length():
+    dfaStates[i] = nfaStates[i] + nfaStates[i+1]
+        
+
+
+
 # ---------------Printing elements of DFA formal description---------------
 # Print a newline to make output look nice and label what is being printed
 print('\n')
 print('The formal description of your DFA is: ')
 
 # Copy over changed elements
-dfaDesc = nfaDesc
-dfaStates = nfaStates
-dfaAlpha = nfaAlpha
-dfaStart = nfaStart
-dfaEnd = nfaEnd
+print(dfaStates)
 print(dfaDesc) 
 
 # Print a newline to make output look nice
